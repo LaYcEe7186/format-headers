3f82106db7c4e26f2fe65db3b6fba45371c00c56
LaYcEe7186 <86745363+LaYcEe7186@users.noreply.github.com>
2023-07-14 00:14:35 +0200

    Rewrite + additional format support

---
+++ README.md
@@ -0,0 +1,7 @@
+# Headers Formatter
+
+This is a script to format HTTP headers into Python dictionary format. Supports both key:value and key\nvalue header formats.
+
+## Usage
+
+To run the program, open cmd in the folder and use the command `python format-headers.py`
--- format-headers.py
+++ format-headers.py
@@ -1,5 +1,33 @@
 import os
 
+def format_headers(input_lines):
+    """
+    Formats headers from a list of input lines.
+
+    Args:
+        input_lines: A list of lines each representing a header key-value pair.
+
+    Returns:
+        A dictionary representing the headers.
+    """
+    headers = {}
+    key = None
+    for line in input_lines:
+        stripped = line.strip()
+        if stripped:
+            if ':' in stripped and ' ' in stripped: # Line format is "key: value"
+                split_line = stripped.split(':', 1) # Only split on the first ':'
+                key = split_line[0].strip().replace(':', '') # Remove all colons from the key
+                value = split_line[1].strip()
+                headers[key] = value
+                key = None # Reset key
+            elif ':' in stripped: # Line format is "key:"
+                key = stripped.replace(':', '')  # Remove all colons from the key
+            elif key: # Line format is "value"
+                headers[key] = stripped # Set value to the key from the previous line
+                key = None # Reset key
+    return headers
+
 def main():
     print('Paste your headers here:')
     lines = []
@@ -9,25 +37,19 @@ def main():
             lines.append(line)
         else:
             break
-
+    
     os.system('cls')
 
-    for line in lines:
-        if line[0] == ':':
-            line = str(line).replace(line[0], '', 1)
+    headers = format_headers(lines)
 
-        line = line[:-1] + line[-1] + "'"
-        line = line[:-1] + line[-1] + ','
-        line_info = str(line).split(': ')[1]
+    print("headers = {")
+    for key, value in headers.items():
+        print(f"    '{key}': '{value}',")
+    print("}")
 
-        line = str(line).replace(line_info, "'" + line_info)
-
-        line_info = line.split(': ')[0]
-        line = line.replace(line_info, "'" + line_info + "'")
-
-        print(line)
-
-while True:
-    main()
-    input('\nPress enter to continue...')
-    os.system('cls')
+if __name__ == "__main__":
+    while True:
+        main()
+        input('\nPress enter to continue...')
+        os.system('cls')
+        
